3c8a8913001840fece0fb0a69dc5ff5d73e2c7fd
Igor <212579997@ge.com>
2017-01-06 21:59:20 +0200

    Second commit

--- read.sh
+++ read.sh
@@ -1,20 +1,31 @@
 #/bin/sh
 
-file="./$1.properties"
+domain=$1
+dec_password=$2
 
-if [ -f "$file" ]
+in=$domain".enc"
+out=$domain".properties"
+
+if [ -f "$in" ]
 then
-  echo "$file found."
+	echo "$in found."
+	openssl enc -aes-256-cbc -d -in $in -out $out -k $dec_password
+  	if [ -f "$out" ]
+  	then
+  		echo "$out found."
+  		while IFS='=' read -r key value
+  		do
+    			key=$(echo $key | tr '.' '_')
+    			eval "${key}='${value}'"
+  			done < "$out"
 
-  while IFS='=' read -r key value
-  do
-    key=$(echo $key | tr '.' '_')
-    eval "${key}='${value}'"
-  done < "$file"
-
-  echo "user name     = " ${username}
-  echo "user password = " ${password}
-  echo "url           = " ${url}
+  			echo "user name     = " ${username}
+  			echo "user password = " ${password}
+  			echo "url           = " ${url}
+		rm -rf $out
+	else
+  		echo "$out not found."
+	fi
 else
-  echo "$file not found."
+	echo "$in not found."
 fi
